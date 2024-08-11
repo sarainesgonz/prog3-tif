12cb6388ab8b4f40fb97c1ceead883c96ef94776
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2024-08-11 19:57:08 -0300

    Adds styles for footer and header

--- InfoSphere/src/components/layout/Footer.jsx
+++ InfoSphere/src/components/layout/Footer.jsx
@@ -1,4 +1,6 @@
+import "../../styles/Footer.css";
+
 function Footer(props) {
-    return <h1>InfoSphere 2024. All rights reserved</h1>;
+    return <h2 className="footer">InfoSphere 2024. All rights reserved</h2>;
   }
 export default Footer;
--- InfoSphere/src/components/layout/Header.jsx
+++ InfoSphere/src/components/layout/Header.jsx
@@ -1,4 +1,11 @@
+import "../../styles/Header.css";
 function Header(props) {
-    return <h1>Wecome to InfoSphere</h1>;
+    return(
+      <div className="header">
+        <img src="../../intelectual_cat_reading.png" alt="img"/>
+        <h1>InfoSphere</h1>
+      </div>
+    )
+
   }
 export default Header;
