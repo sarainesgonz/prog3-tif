7f522734526d03d7c280ebe73e9cfbe9406f61b2
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2024-08-11 21:03:36 -0300

    adds functionality to the navbar

--- InfoSphere/src/components/layout/NavBar.jsx
+++ InfoSphere/src/components/layout/NavBar.jsx
@@ -1,11 +1,20 @@
 import "../../styles/NavBar.css";
+import { useNavigate } from "react-router-dom";
 
 function NavBar(props) {
+    const navigate = useNavigate() //navigate para cambiar de pagina una vez autenticado
+    const handleClick = (e) => {
+        if(e.target.textContent === "Noticias"){
+            navigate("/")
+        } else if (e.target.textContent === "Login"){
+            navigate("/login")
+        }
+    }
     return (
     <div>
         <ul className="options">
-            <li>Noticias</li>
-            <li>Login</li>
+            <li onClick={handleClick}>Noticias</li>
+            <li onClick={handleClick}>Login</li>
         </ul>
     </div>
 )
