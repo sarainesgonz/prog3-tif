af8908671a84afcdd8fcac824cd68f4cd162c9bb
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2025-01-02 21:37:48 -0300

    added not found page

--- InfoSphere/src/components/NotFoundPage.jsx
+++ InfoSphere/src/components/NotFoundPage.jsx
@@ -2,8 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NotFoundPage(props) {
     return (
-        <div>
-            <h1>Page Not Found</h1>
+        <div className='d-flex flex-column justify-content-center align-items-center vh-100' >
+            <h1 className='display-1 text-center mb-4'>404</h1>
+            <h2 className='text-center'>Page Not Found</h2>
         </div>
     )
 }
